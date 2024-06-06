8fb35df6b9af6929b2f8c4b72e967a1dabd6374d
joel <joelchiang2k@gmail.com>
2024-06-05 20:42:52 -0400

    react

---
+++ text-to-speech-converter/backend/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+console.log('Environment PORT:', process.env.PORT);
+console.log('Using PORT:', port);
+
+// Use cors middleware to enable CORS for specific domains
+const allowedOrigins = ['http://localhost:3000', 'https://your-frontend-domain.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+console.log(process.env.AWS_ACCESS_KEY_ID)
+console.log(process.env.AWS_SECRET_ACCESS_KEY)
+// Configure AWS credentials and Polly
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-2'
+});
+
+const polly = new AWS.Polly();
+
+// Middleware setup
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//post request sent from frontend
+console.log(port)
+app.post('/synthesize', async (req, res) => {
+    const text = req.body.text;
+    const selectedVoice = req.body.voice || 'Joanna'; 
+    if (!text) {
+      return res.status(400).json({ error: 'Text not provided' });
+    }
+    
+    //params sent from frontend
+    const params = {
+      OutputFormat: 'mp3',
+      Text: text,
+      VoiceId: selectedVoice
+    };
+    
+    //sends mpeg file to frontend to play
+    try {
+      const data = await polly.synthesizeSpeech(params).promise();
+      res.set('Content-Type', 'audio/mpeg');
+      res.send(data.AudioStream);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Speech synthesis failed' });
+    }
+  });
+
+//running port
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
