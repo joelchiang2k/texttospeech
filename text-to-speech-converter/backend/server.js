3a9bc84f364511db7f4533fb36451383acb63362
joel <joelchiang2k@gmail.com>
2024-06-05 22:20:56 -0400

    changes

--- text-to-speech-converter/backend/server.js
+++ text-to-speech-converter/backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 console.log('Environment PORT:', process.env.PORT);
 console.log('Using PORT:', port);
